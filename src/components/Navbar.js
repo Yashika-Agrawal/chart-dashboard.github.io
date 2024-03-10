244da3a01180ecdd29c1a55212c5602225f53c1a
Yashika Agrawal <97408966+Yashika-Agrawal@users.noreply.github.com>
2024-03-11 03:02:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,27 @@ import logo from "../profile icon.png";
 const Navbar = ({ isDarkMode, toggleDarkMode }) => {
   return (
     <div
-      className={`bg-gray-800 text-white p-4 flex justify-between items-center ${
+      className={`bg-gray-800 text-white p-4 xxl:p-5 flex justify-between items-center  ${
         isDarkMode ? "dark-mode" : "light-mode"
       }`}
+      s
     >
-      <div className="text-2xl font-bold">Dashboard</div>
+      <div className="text-xl font-bold xxl:text-xl">Dashboard</div>
       <div className="flex items-center">
-        <div className="mr-2">Yashika Agrawal</div>
-        <img
-          src={logo}
-          alt="User Profile"
-          className="cursor-pointer w-8 h-8 rounded-full"
-          
-        />
-        <div className="ml-2">
+        <div className="mr-2 2xl:mr-5">
           <button
             onClick={toggleDarkMode}
-            className="text-sm font-bold border-2 border-white px-5 py-3 rounded-full hover:bg-white hover:text-black"
+            className="text-sm xxl:text-lg font-bold border-2 border-white md:px-5 px-2 md:py-3 py-2  rounded-full hover:bg-white hover:text-black"
           >
             {isDarkMode ? "Light Mode" : "Dark Mode"}
           </button>
         </div>
+        <div className="mr-2 xxs:hidden">Yashika Agrawal</div>
+        <img
+          src={logo}
+          alt="User Profile"
+          className="cursor-pointer w-8 h-8 rounded-full xxl:w-11 xxl:h-11 "
+        />
       </div>
     </div>
   );
